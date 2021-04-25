e25db022eefc92206db6584a4e35265bf4b0d300
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-04-26 01:12:35 +0200

    Add project files.

---
+++ BlazorDragAndDrop/Shared/TaskModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDragAndDrop.Shared
+{
+    public class TaskModel
+    {
+        public int ID { get; set; }
+        public TaskStatuses Status { get; set; }
+        public string Detail { get; set; }
+        public DateTime UpdatedTime { get; set; }
+
+    }
+
+    public enum TaskStatuses
+    {
+        Todo,
+        Started,
+        Completed,
+    }
+}
